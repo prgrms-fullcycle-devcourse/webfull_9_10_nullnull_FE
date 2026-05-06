0f4e3f09359cb22cd8410701c9be7bd72591f503
cmlee7796-stack <cmlee7796@gmail.com>
2026-05-06 22:00:39 +0900

    fix : 지도 중복 블록 제거

--- src/types/kakao.d.ts
+++ src/types/kakao.d.ts
@@ -1,7 +1,27 @@
+type KakaoLatLng = object;
+type KakaoLatLngBounds = { extend: (latlng: KakaoLatLng) => void };
+type KakaoMap = { setBounds: (bounds: KakaoLatLngBounds) => void };
+type KakaoMarker = object;
+type KakaoInfoWindow = { open: (map: KakaoMap, marker: KakaoMarker) => void };
+
 interface Window {
   kakao: {
     maps: {
       load: (callback: () => void) => void;
+      Map: new (
+        container: HTMLElement,
+        options: { center: KakaoLatLng; level: number },
+      ) => KakaoMap;
+      LatLng: new (lat: number, lng: number) => KakaoLatLng;
+      LatLngBounds: new () => KakaoLatLngBounds;
+      Marker: new (options: {
+        position: KakaoLatLng;
+        map: KakaoMap;
+      }) => KakaoMarker;
+      InfoWindow: new (options: {
+        content: string;
+        removable?: boolean;
+      }) => KakaoInfoWindow;
       services: {
         Places: new () => {
           keywordSearch: (
