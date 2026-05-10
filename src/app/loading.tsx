8bfc1dfebc680a9f9f00b5d3888d78470b6977bb
yeonhee <yiyomiyoni@gmail.com>
2026-05-11 01:59:10 +0900

    style : 공통 로딩 UI 추가

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import { LoadingView } from "@/components/visual/LoadingView";
+
+export default function LoadingPage() {
+  return (
+    <div className="Wrap">
+      <div className="wrap-container">
+        <LoadingView />
+      </div>
+    </div>
+  );
+}
