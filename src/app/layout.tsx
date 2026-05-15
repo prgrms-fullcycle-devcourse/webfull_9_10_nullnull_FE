6ea2846fef0e3a4308c29feede2b21a4100fec9b
yeonhee <yiyomiyoni@gmail.com>
2026-05-15 16:42:12 +0900

    fix : og이미지가로사이즈수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/og-image.svg",
-        width: 1024,
-        height: 559,
+        width: 1200,
+        height: 630,
         alt: "NULLNULL",
       },
     ],
